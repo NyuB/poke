eb99d139572ac9806ca6844690ba4191b35c0c4b
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-10-18 13:10:17 +0200

    Remove unused class

--- src/main/kotlin/nyub/poke/MultiValueTask.kt
+++
@@ -1,9 +0,0 @@
-package nyub.poke
-
-import nyub.poke.Kontext.Companion.one
-
-class MultiValueTask(private val values: Map<NodeKey, Any>) : Task {
-  override fun Kontext.execute(): Map<NodeKey, Kontext.K<*>> {
-    return values.mapValues { one { it } }
-  }
-}
