64e2e20b9899fe445d90694c6fe181fa49ae0db1
Brice Decaestecker <brice.decaestecker@gmx.fr>
2025-02-07 13:12:55 +0100

    exhibit caching problem

--- src/main/kotlin/nyub/poke/TaskGraph.kt
+++ src/main/kotlin/nyub/poke/TaskGraph.kt
@@ -32,31 +32,31 @@ private class IsolatedTask(
     private val links: Map<InputId, Pair<TaskId, OutputId>>
 ) : Execution, Task by task {
 
-    fun ensureExecutable() {
-        Dependencies.track(task).forEach { dep ->
-            val (taskId, outputId) =
-                links[dep.key] ?: throw IllegalArgumentException("Input ${dep.key} is not linked")
-            val task =
-                tasks(taskId)
-                    ?: throw IllegalArgumentException("Task ${taskId} is missing in the task graph")
-            val output =
-                task.task.describe()[outputId]
-                    ?: throw IllegalArgumentException("Task $taskId does not define an output $outputId")
-            require(dep.type.isAssignableFrom(output.type)) {
-                "Output $outputId has type ${output.type} which is incompatible with type ${dep.type} required for input ${dep.key}"
-            }
-        }
+  fun ensureExecutable() {
+    Dependencies.track(task).forEach { dep ->
+      val (taskId, outputId) =
+          links[dep.key] ?: throw IllegalArgumentException("Input ${dep.key} is not linked")
+      val task =
+          tasks(taskId)
+              ?: throw IllegalArgumentException("Task ${taskId} is missing in the task graph")
+      val output =
+          task.task.describe()[outputId]
+              ?: throw IllegalArgumentException("Task $taskId does not define an output $outputId")
+      require(dep.type.isAssignableFrom(output.type)) {
+        "Output $outputId has type ${output.type} which is incompatible with type ${dep.type} required for input ${dep.key}"
+      }
     }
+  }
 
-    override fun <A : Any> executeFetch(fetch: Description.Fetch<A>): Try<A> =
-        Try.attempt {
+  override fun <A : Any> executeFetch(fetch: Description.Fetch<A>): Try<A> =
+      Try.attempt {
             val (taskId, outputId) = links[fetch.key]!!
             tasks(taskId)!!.executeOutput(outputId, fetch.type)
-        }
-            .flatten()
+          }
+          .flatten()
 
-    private fun <A> executeOutput(outputId: OutputId, type: Class<A>): Try<A> =
-        Try.attempt { execute(task.describe()[outputId]!!) }
-            .flatten()
-            .flatMap { Try.attempt { type.cast(it) } }
+  private fun <A> executeOutput(outputId: OutputId, type: Class<A>): Try<A> =
+      Try.attempt { execute(task.describe()[outputId]!!) }
+          .flatten()
+          .flatMap { Try.attempt { type.cast(it) } }
 }
--- src/test/kotlin/nyub/poke/TaskGraphTest.kt
+++ src/test/kotlin/nyub/poke/TaskGraphTest.kt
@@ -2,6 +2,7 @@ package nyub.poke
 
 import nyub.poke.Description.Combine.Companion.combine
 import nyub.poke.Description.Fetch.Companion.fetch
+import nyub.poke.Description.Map.Companion.map
 import nyub.poke.Description.One.Companion.one
 import org.junit.jupiter.api.Test
 
@@ -54,6 +55,28 @@ class TaskGraphTest : WithAssertExtensions {
                         "separator" to "comma"["str"],
                     )))
     graph.execute("concat") `is equal to` mapOf("result" to Try.success("A,B"))
+  }
+
+  @Test
+  fun cachingProblem() {
+    class Callable {
+      var count: Int = 0
+
+      fun call(): Int {
+        ++count
+        return 1
+      }
+    }
+
+    val root = Callable()
+    val rootCall = one { root.call() }
+    val twice = rootCall.map { it * 2 }
+    val thrice = rootCall.map { it * 3 }
+
+    val task = Task { mapOf("twice" to twice, "thrice" to thrice) }
+    val graph = TaskGraph(mapOf("task" to task), emptyMap())
+    graph.execute("task") `is equal to` mapOf("twice" to Try.success(2), "thrice" to Try.success(3))
+    root.count `is equal to` 2
   }
 
   operator fun String.get(key: String): Pair<String, String> = this to key
